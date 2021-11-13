1fbc7ee0b2ef12a337c9fb815877476ef66e0420
yuu0329 <yuukss0329@gmail.com>
2021-11-13 14:13:06 +0900

    ページ遷移機能追加

--- pro-wrestling_schedule.py
+++ pro-wrestling_schedule.py
@@ -1,26 +1,44 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import time
 
 HEADER = ['開催日','開場時間','開始時間','会場']
-
 url = 'https://www.njpw.co.jp/schedule'
-res = requests.get(url)
 
-soup = BeautifulSoup(res.content, 'html.parser')
-events = soup.find_all('div',class_='eventInfo')
-
-with open('dates.csv','w',encoding='utf_8_sig') as f:
+with open('njpw_events.csv','w',encoding='utf_8_sig') as f:
 
     writer = csv.writer(f)
     writer.writerow(HEADER)
 
-    for event in events:
-        eventInfo = (event.find_all('dd'))
-        date = eventInfo[0].text
-        place = eventInfo[1].text
+while True:
 
-        row = date.split()
-        row.append(place)
+    res = requests.get(url)
+    res.raise_for_status()
 
-        writer.writerow(row)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    events = soup.find_all('div',class_='eventInfo')
+    next_page = soup.find('li',class_='next')
+
+    print(next_page)
+
+    with open('njpw_events.csv','a',encoding='utf_8_sig') as f:
+
+        writer = csv.writer(f)
+
+        for event in events:
+            eventInfo = (event.find_all('dd'))
+            date = eventInfo[0].text
+            place = eventInfo[1].text
+
+            row = date.split()
+            row.append(place)
+
+            writer.writerow(row)
+    
+    
+    if bool(next_page) == False:
+        break
+    
+    url = next_page.a.get("href")
+    time.sleep(1)
