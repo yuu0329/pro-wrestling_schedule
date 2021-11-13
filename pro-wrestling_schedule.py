11f038d1cdb5453f80b69c606cf6de2cfe89d7d3
yuu0329 <yuukss0329@gmail.com>
2021-11-13 13:29:13 +0900

    会場情報追加

--- pro-wrestling_schedule.py
+++ pro-wrestling_schedule.py
@@ -1,14 +1,13 @@
-from os import write
 from bs4 import BeautifulSoup
 import requests
 import csv
 
-HEADER = ['開催日','開場時間','開始時間']
+HEADER = ['開催日','開場時間','開始時間','会場']
 
 url = 'https://www.njpw.co.jp/schedule'
 res = requests.get(url)
 
-soup = BeautifulSoup(res.text, "html.parser")
+soup = BeautifulSoup(res.content, 'html.parser')
 events = soup.find_all('div',class_='eventInfo')
 
 with open('dates.csv','w',encoding='utf_8_sig') as f:
@@ -17,8 +16,11 @@ with open('dates.csv','w',encoding='utf_8_sig') as f:
     writer.writerow(HEADER)
 
     for event in events:
-        date = event.find('dd').text
+        eventInfo = (event.find_all('dd'))
+        date = eventInfo[0].text
+        place = eventInfo[1].text
 
         row = date.split()
-        
+        row.append(place)
+
         writer.writerow(row)
